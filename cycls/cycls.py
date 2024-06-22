dee219fadeeb3c3fab08fbbe8f159847d7d3b7e9
Mohammed Jamal <58256600+aburjg@users.noreply.github.com>
2024-06-22 19:21:39 +0000

    dropped ssh binary dependency for asyncssh

--- cycls/cycls.py
+++ cycls/cycls.py
@@ -1,18 +1,15 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import StreamingResponse, PlainTextResponse
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
+from typing import List, Dict, Optional
 
 from functools import wraps
 import uvicorn, socket, httpx
 import inspect
 
-from concurrent.futures import ThreadPoolExecutor
-import time, subprocess
-
 import logging
 logging.basicConfig(level=logging.ERROR)
 
-from typing import List, Dict, Optional
 class Message(BaseModel):
     handle: str
     content: str
@@ -26,6 +23,33 @@ def find_available_port(start_port):
             if s.connect_ex(('localhost', port)) != 0:
                 return port
             port += 1
+
+import asyncssh, asyncio
+async def create_ssh_tunnel(x,y,z='tuns.karpov.solutions'):
+    try:
+        async with asyncssh.connect(z,port=2222,known_hosts=None) as conn:
+            await conn.forward_remote_port(x, 80, 'localhost', y)
+            print("✦/✧","tunnel established\n")
+            await asyncio.Future()  # run forever
+    except (OSError, asyncssh.Error) as e:
+        print("✦/✧",f"tunnel disconnected: {e}")
+
+def register(handle, network, url, mode):
+    try:
+        with httpx.Client() as client:
+            response = client.post(f"{network}/register", json={"handle":f"@{handle}", "url":url, "mode":mode})
+            if response.status_code==200:
+                print(f"✦/✧ published 🎉")
+                print("")
+            else:
+                print("✦/✧ failed to register ⚠️")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+async def run_server(x,y):
+    config = uvicorn.Config(x, host="127.0.0.1", port=y, log_level="error")
+    server = uvicorn.Server(config)
+    await server.serve()
 
 class Cycls:
     def __init__(self,network="https://cycls.com", port=find_available_port(8001),url=""):
@@ -43,18 +67,18 @@ class Cycls:
                 async def async_wrapper(*args, **kwargs):
                     return await func(*args, **kwargs)
                 self.server.post('/main')(async_wrapper)
-                self.publish()
+                asyncio.run(self.publish())
                 return async_wrapper
             else:
                 @wraps(func)
                 def sync_wrapper(*args, **kwargs):
                     return func(*args, **kwargs)
                 self.server.post('/main')(sync_wrapper)
-                self.publish()
+                asyncio.run(self.publish())
                 return sync_wrapper
         return decorator
 
-    def publish(self):
+    async def publish(self):
         prod=False
         if self.url != "":
             prod=True
@@ -65,35 +89,11 @@ class Cycls:
         else:
             self.url = f"http://{self.handle}-cycls.tuns.karpov.solutions"
             print("✦/✧","development mode\n")
+            t2 = asyncio.create_task(run_server(self.server,self.port))
             
         print("✦/✧",f"visit app: {self.network}/@{self.handle}","\n")
 
-        with ThreadPoolExecutor() as executor:
-            if not prod:
-                self.register('dev')
-                executor.submit(self.tunnel)
-            else:
-                self.register('prod')
-
-            executor.submit(uvicorn.run(self.server, host="127.0.0.1", port=self.port, log_level="error")) # perhaps keep traces?
-
-    def register(self, mode):
-        try:
-            with httpx.Client() as client:
-                response = client.post(f"{self.network}/register", json={"handle": f"@{self.handle}", "url": self.url, "mode": mode})
-                if response.status_code==200:
-                    print(f"✦/✧ published 🎉")
-                    print("")
-                else:
-                    print("✦/✧ failed to register ⚠️")
-        except Exception as e:
-            print(f"An error occurred: {e}")
-
-    def tunnel(self):
-        ssh_command = ['ssh', '-o', 'StrictHostKeyChecking=no', '-p', '2222', '-R', f'{self.handle}-cycls:80:localhost:{self.port}', 'tuns.karpov.solutions']
-        try:
-            process = subprocess.run(ssh_command,stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
-        except Exception as e:
-            print(f"ssh error: {e}")
+        t1 = asyncio.create_task(create_ssh_tunnel(f"{self.handle}-cycls", self.port))
+        await asyncio.gather(t1, t2) if not prod else asyncio.gather(t2)
 
 Text = StreamingResponse
