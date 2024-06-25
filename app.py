341a5101b043615efe1a6e76cac5c16c02bfcbb4
Mohammed Jamal <58256600+aburjg@users.noreply.github.com>
2024-06-25 20:40:42 +0000

    agent calls

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 from cycls import Cycls
 
-# network = "https://7c0ed7ef-03ed-48bd-8eda-a6a9a5e846e2-00-2bs5oneddesjb.picard.replit.dev"
-# cycls = Cycls(network=network)
+graph = "https://7c0ed7ef-03ed-48bd-8eda-a6a9a5e846e2-00-2bs5oneddesjb.picard.replit.dev"
+cycls = Cycls(graph=graph)
 
-cycls = Cycls()
+# cycls = Cycls()
 
-@cycls("bake")
+@cycls("@bake")
 def bake_app(x):
-    print("history",x.history)
-    print("session id",x.id)
+    print("history", x.history)
+    print("session id", x.id)
     return x.content + " bake"
 
-@cycls("take")
+@cycls("@take")
 async def take_app(x):
-    print("history",x.history)
-    print("session id",x.id)
+    print("history", x.history)
+    print("session id", x.id)
     return x.content + " take"
 
+@cycls("@op")
+async def agent_app(x):
+    return cycls.call("@groq", x.content) # why not await?
+    
+# looking for parity with OpenAI/Groq streams!...
 cycls.push()
+
--- cycls/cycls.py
+++ cycls/cycls.py
@@ -10,6 +10,8 @@ import inspect
 import logging
 logging.basicConfig(level=logging.ERROR)
 
+O = lambda x,y: print(f"✦/✧ {str(x).ljust(7)} | {y}")
+
 import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 key_path = os.path.join(current_dir, 'tuns')
@@ -20,6 +22,7 @@ class Message(BaseModel):
     id: str
     history: Optional[List[Dict[str, str]]] = None
 
+#!
 def find_available_port(start_port):
     port = start_port
     while True:
@@ -29,39 +32,34 @@ def find_available_port(start_port):
             port += 1
 
 import asyncssh, asyncio
-
 async def create_ssh_tunnel(x,y,z='tuns.sh'):
     try:
         async with asyncssh.connect(z,client_keys=[key_path],known_hosts=None) as conn:
             listener = await conn.forward_remote_port(x, 80, 'localhost', y)
-            print("✦/✧","tunnel\n")
+            O("tunnel","open");print(" ")
             await listener.wait_closed()
     except (OSError, asyncssh.Error) as e:
-        print("✦/✧",f"tunnel disconnected: {e}")
+        O("tunnel",f"disconnected ({e})")
 
-def register(handles, network, url, mode):
+def register(handles, graph, url, mode):
     try:
         with httpx.Client() as client:
-            response = client.post(f"{network}/register", json={"handles":handles, "url":url, "mode":mode})
+            response = client.post(f"{graph}/register", json={"handles":handles, "url":url, "mode":mode})
             if response.status_code==200:
                 data = (response.json()).get("content")
-                for i in data:print(f"✦/✧ {i[0]} / {network}/{i[1]}")
+                for i in data:
+                    O(i[0],f"{graph}/{i[1]}")
             else:
                 print("✦/✧ failed to register ⚠️")
     except Exception as e:
         print(f"An error occurred: {e}")
 
-async def run_server(x,y):
-    config = uvicorn.Config(x, host="127.0.0.1", port=y, log_level="error")
-    server = uvicorn.Server(config)
-    await server.serve()
-
 class Cycls:
-    def __init__(self, url="", network="https://cycls.com", port=find_available_port(8001)):
+    def __init__(self, url="", graph="https://cycls.com", port=find_available_port(8001)):
         import uuid
         self.subdomain = str(uuid.uuid4())[:8]
         self.server = FastAPI()
-        self.network = network
+        self.graph = graph
         self.port = port
         self.url = url
         self.apps = {}
@@ -75,7 +73,7 @@ class Cycls:
             def sync_wrapper(*args, **kwargs):
                 return StreamingResponse(func(*args, **kwargs))
             wrapper = async_wrapper if inspect.iscoroutinefunction(func) else sync_wrapper
-            self.apps["@"+handle] = wrapper
+            self.apps[handle] = wrapper
             return wrapper
         return decorator
     
@@ -90,26 +88,42 @@ class Cycls:
 
     def push(self):
         self.server.post("/gateway")(self.gateway)
-        asyncio.run(self.publish())
+        @self.server.on_event("startup")
+        def startup_event():
+            asyncio.create_task(create_ssh_tunnel(f"{self.subdomain}-cycls", self.port))
 
-    async def publish(self):
+        self.publish()
+
+        try:
+            uvicorn.run(self.server, host="127.0.0.1", port=self.port, log_level="error")
+        except KeyboardInterrupt:
+            print(" ");O("exit","done")
+
+    def publish(self):
+        #!
         prod=False
         if self.url != "":
             prod=True
 
-        print(f"✦/✧ serving at port: {self.port}")
+        O("port",self.port)
         if prod:
-            print("✦/✧",f"production mode | url: {self.url}")
-            register(list(self.apps.keys()), self.network, self.url+"/gateway", "prod")
+            O("mode",f"production (url:{self.url})")
+            register(list(self.apps.keys()), self.graph, self.url+"/gateway", "prod")
         else:
             self.url = f"http://{self.subdomain}-cycls.tuns.sh"
-            print("✦/✧","development mode")
-            # print("✦/✧",f"url {self.url}")
-            register(list(self.apps.keys()), self.network, self.url+"/gateway", "dev")
-            t1 = asyncio.create_task(create_ssh_tunnel(f"{self.subdomain}-cycls", self.port))
+            O("mode","development")
+            register(list(self.apps.keys()), self.graph, self.url+"/gateway", "dev")
 
-        t2 = asyncio.create_task(run_server(self.server,self.port))
+    async def call(self, handle, content):
+        data = {"handle":handle, "content":content, "session":{}, "agent":"yes"}
+        try:
+            url = f"{self.graph}/stream/"
+            async with httpx.AsyncClient(timeout=20) as client, client.stream("POST", url, json=data) as response:
+                if response.status_code != 200:
+                    print("http error")
+                async for token in response.aiter_text():
+                    yield token
+        except Exception as e:
+            print("Exception", e)
         
-        await asyncio.gather(t1, t2) if not prod else await asyncio.gather(t2)
-
 # poetry publish --build
